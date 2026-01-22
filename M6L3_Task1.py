80e1d127d13ba8859baf156746fb5816853792a9
Made Acarya MK <150651906+cat-ninja007@users.noreply.github.com>
2026-01-22 16:59:03 +0800

    Add initial questions for database analysis
    Add questions and solution options for database analysis.

---
+++ M6L3_Task1.py
@@ -0,0 +1,13 @@
+ #Question #1. Berapa banyak artis yang terwakili dalam database? 
+ 
+#Question #2. Berapa banyak perempuan (Female) yang ada dalam database?
+ 
+#Question #3. Berapa banyak orang dalam database yang lahir sebelum tahun 1900?
+ 
+#Question #4*. Siapa nama artis tertua?
+
+
+#Opsi solusi #1: gunakan alat Python standar
+
+ 
+#Opsi solusi #2: SQL saja
